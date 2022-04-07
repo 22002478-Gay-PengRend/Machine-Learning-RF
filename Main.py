018cab1dbef784ed175769c608cbf7d48c1c45cf
22002478-Gay-PengRend <99471712+22002478-Gay-PengRend@users.noreply.github.com>
2022-04-07 23:25:04 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -91,7 +91,7 @@ if st.button('Make Prediction'):
     st.write(f"Thank you {st.session_state.name} for participating!")
     
 if st.checkbox('Show Confusion Matrix'):
-    image = Image.open('Confusion_Matrix.jpg')
+    image = Image.open('https://github.com/22002478-Gay-PengRend/Machine-Learning-RF/blob/main/Confusion_Matrix.JPG')
     st.image(image, caption='Confusion Matrix') 
     
 
